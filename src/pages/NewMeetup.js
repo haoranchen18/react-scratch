1bcc4191d5dead18b333bd5fbf0bc422a6493b9e
haoranchen18 <haoran.chen18@gmail.com>
2022-05-25 13:51:24 -0700

    navigating programmatically

--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,9 @@
+import { useHistory } from "react-router-dom";
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetup() {
+  // 这也是个hook，来控制页面访问的history，从而达到返回上一个页面，或跳转到其他页面的功能
+  const history = useHistory();
+  
   function addMeetupHandler(meetupData) {
     fetch(
       "https://react-getting-started-ea358-default-rtdb.firebaseio.com/meetups.json",
@@ -8,9 +12,12 @@ function NewMeetup() {
         body: JSON.stringify(meetupData),
         headers: {
           "Content-Type": "application/json",
-        },
+        }
       }
-    );
+    ).then(() => {
+      // submit 这个 form 之后，页面会跳转到 All Meetups 这个主页
+      history.replace('/');
+    });
   }
 
   return (
