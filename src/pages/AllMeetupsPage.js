ef1b29b9c05f3a525c4e6407083ccd40e9cc0659
haoranchen18 <haoran.chen18@gmail.com>
2022-05-25 14:55:54 -0700

    fetching data in hooks, useState, useEffect

--- src/pages/AllMeetupsPage.js
+++ src/pages/AllMeetupsPage.js
@@ -1,31 +1,46 @@
 // this componenet be responsible for loading and displaying all the
 // meetups that we have
-
-
+import { useState, useEffect } from "react";
 import MeetupList from "../components/meetups/MeetupList";
 
-const DUMMY_DATA = [
-  {
-    id: 'm1',
-    title: 'This is a first meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-  {
-    id: 'm2',
-    title: 'This is a second meetup',
-    image:
-      'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/2560px-Stadtbild_M%C3%BCnchen.jpg',
-    address: 'Meetupstreet 5, 12345 Meetup City',
-    description:
-      'This is a first, amazing meetup which you definitely should not miss. It will be a lot of fun!',
-  },
-];
-
 function AllMeetupsPage() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [loadedMeetups, setLoadedMeetups] = useState([]);
+
+  useEffect(() => {
+    setIsLoading(true);
+    // fetch 会 return 一个 promise
+    fetch(
+      "https://react-getting-started-ea358-default-rtdb.firebaseio.com/meetups.json"
+    )
+      .then((response) => {
+        // .json() 又会return 一个 promise
+        return response.json();
+      })
+      .then((data) => {
+        // data 自己是一个大object包过小的objects，而我们要的内容是data里面的object
+        const meetups = [];
+        for (const key in data) {
+          const meetup = {
+            id: key,
+            ...data[key],
+          };
+          meetups.push(meetup);
+        }
+
+        setIsLoading(false);
+        setLoadedMeetups(meetups);
+      });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
   return (
     <section>
       <h1>All Meetups</h1>
@@ -35,8 +50,7 @@ function AllMeetupsPage() {
           return <li key={meetup.id}>{meetup.title}</li>;
         })}
       </ul> */}
-      <MeetupList meetups={DUMMY_DATA}>
-      </MeetupList>
+      <MeetupList meetups={loadedMeetups}></MeetupList>
     </section>
   );
 }
