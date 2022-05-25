7bc60f35f4b30bbc63f7a954cb8aa5c132a17b6e
haoranchen18 <haoran.chen18@gmail.com>
2022-05-25 00:28:17 -0700

    Getting User input and handling form submission, cue DOM in react

---
+++ src/components/meetups/NewMeetupForm.js
@@ -0,0 +1,59 @@
+import { useRef } from "react";
+import Card from "../ui/Card";
+import classes from "./NewMeetupForm.module.css";
+
+function NewMeetupForm() {
+  const titleInputRef = useRef();
+  const imageInputRef = useRef();
+  const addressInputRef = useRef();
+  const descriptionInputRef = useRef();
+
+  function submitHandler(event) {
+    // browser default 情况是onSubmit之后，给server 发request，返回新page
+    // 但我们不想。所以要用event 内置的 preventDefault method 去阻止
+    event.preventDefault();
+
+    // 用ref 得到当前input 输入的东西 在 value property中
+    const enteredTitle = titleInputRef.current.value;
+    const enteredImage = imageInputRef.current.value;
+    const enteredAddress = addressInputRef.current.value;
+    const enteredDescription = descriptionInputRef.current.value;
+
+    const meetupData = {
+        title: enteredTitle,
+        image: enteredImage,
+        address: enteredAddress,
+        description: enteredDescription
+    };
+
+    console.log(meetupData);
+  }
+
+  return (
+    <Card>
+      <form className={classes.form} onSubmit={submitHandler}>
+        <div className={classes.control}>
+          <label htmlFor="title">Meetup Title</label>
+          <input type="text" required id="title" ref={titleInputRef} />
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="image">Meetup Image</label>
+          <input type="url" required id="image" ref={imageInputRef}/>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="address">Address</label>
+          <input type="text" required id="address" ref={addressInputRef}/>
+        </div>
+        <div className={classes.control}>
+          <label htmlFor="description">Description</label>
+          <textarea id="description" required rows="5" ref={descriptionInputRef}></textarea>
+        </div>
+        <div className={classes.actions}>
+          <button>Add Meetup</button>
+        </div>
+      </form>
+    </Card>
+  );
+}
+
+export default NewMeetupForm;
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,5 +1,11 @@
+import NewMeetupForm from "../components/meetups/NewMeetupForm";
 function NewMeetup() {
-  return <div>New Meetups page</div>;
+  return (
+    <section>
+      <h1>Add New Meetup</h1>
+      <NewMeetupForm />
+    </section>
+  );
 }
 
 export default NewMeetup;
