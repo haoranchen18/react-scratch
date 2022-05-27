b95ec265f8083ceb58e03a8de698d4e33f3195f5
haoranchen18 <haoran.chen18@gmail.com>
2022-05-27 15:02:14 -0700

    useRef, useEffect cleanup

--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 import Card from "../UI/Card";
 import "./Search.css";
@@ -8,27 +8,42 @@ const Search = React.memo((props) => {
   const { onLoadIngredients } = props;
   const [enteredFilter, setEnteredFilter] = useState("");
 
+  const inputRef = useRef();
+
   useEffect(() => {
-    const query =
-      enteredFilter.length === 0
-        ? ""
-        : `?orderBy="title"&equalTo="${enteredFilter}"`;
-    fetch('https://react-hooks-update-48230-default-rtdb.firebaseio.com/ingredients.json' + query)
-      .then((response) => response.json())
-      .then((responseData) => {
-        const loadedIngredients = [];
-        for (const key in responseData) {
-          loadedIngredients.push({
-            id: key,
-            title: responseData[key].title,
-            amount: responseData[key].amount,
+    const timer = setTimeout(() => {
+      if (enteredFilter === inputRef.current.value) {
+        const query =
+          enteredFilter.length === 0
+            ? ""
+            : `?orderBy="title"&equalTo="${enteredFilter}"`;
+        fetch(
+          "https://react-hooks-update-48230-default-rtdb.firebaseio.com/ingredients.json" +
+            query
+        )
+          .then((response) => response.json())
+          .then((responseData) => {
+            const loadedIngredients = [];
+            for (const key in responseData) {
+              loadedIngredients.push({
+                id: key,
+                title: responseData[key].title,
+                amount: responseData[key].amount,
+              });
+            }
+            onLoadIngredients(loadedIngredients);
           });
-        }
-       onLoadIngredients(loadedIngredients);
-      });
-      // onLoadIngredients 如果在parent component 中，ie. Ingredients 中，用useCallback，那么这个地方
-      // 的值也理所当然的不会变，所以[enteredFilter, onLoadIngredients] 都没有改变，则useEffect只运行一次
-      // 所以就不会有infinite loop
+      }
+    }, 500);
+
+    // useEffect cleanup
+    return () => {
+      clearTimeout(timer);
+    };
+
+    // onLoadIngredients 如果在parent component 中，ie. Ingredients 中，用useCallback，那么这个地方
+    // 的值也理所当然的不会变，所以[enteredFilter, onLoadIngredients] 都没有改变，则useEffect只运行一次
+    // 所以就不会有infinite loop
   }, [enteredFilter, onLoadIngredients]);
 
   return (
@@ -37,6 +52,7 @@ const Search = React.memo((props) => {
         <div className="search-input">
           <label>Filter by Title</label>
           <input
+            ref={inputRef}
             type="text"
             value={enteredFilter}
             onChange={(event) => setEnteredFilter(event.target.value)}
